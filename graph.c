a7bd1b8ae5177a6bfeb6facacfd1218e1f2485b6
hapi666 <281127500@qq.com>
2019-02-21 13:04:23 +0800

    有向图的深度优先搜索和最短路径

--- graph.c
+++ graph.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define INF	(~(0x1<<31))
 /*
 	图
 */
 struct BNode
 {
 	int data;
+	int weight;
 	struct BNode *next;
 };
 
@@ -41,6 +43,7 @@ void insertNode(struct BNode *g, struct BNode *node){
 	printf("被插入数据：%d\n", temp->next->data);
 }
 
+// 创建有向图
 struct Graph* creatGraph(){
 	int i = 0;
 	struct Graph *g = (struct Graph*)malloc(sizeof(struct Graph));
@@ -70,9 +73,10 @@ struct Graph* creatGraph(){
 	printf("请输入顶点之间的关系：（即相连顶点，以逗号分隔）\n");
 	for(i = 0; i < g->sidnum; i++){
 		char ch1, ch2;
+		int w = 0;
 		getchar();
-		scanf("%c,%c", &ch1, &ch2);
-		printf("%c,%c\n", ch1,ch2);
+		scanf("%c, %c, %d", &ch1, &ch2, &w);
+		printf("%c, %c, %d\n", ch1, ch2, w);
 		struct BNode *node1, *node2;
 		node1 = (struct BNode*)malloc(sizeof(struct BNode));
 		if(node1 == NULL){
@@ -86,6 +90,7 @@ struct Graph* creatGraph(){
 		}
 		node1->data = getPosition(ch1, g);
 		node2->data = getPosition(ch2, g);
+		node2->weight = w;
 		node1->next = NULL;
 		node2->next = NULL;
 		if(g->node[node1->data]->first_node == NULL){
@@ -122,10 +127,99 @@ void printGraph(struct Graph *g){
 	}
 }
 
+// 深度优先搜索
+void DFS(struct Graph *g, int i, int *visited){
+	visited[i] = 1;
+	printf("%c\n", g->node[i]->ch);
+	struct BNode *n = g->node[i]->first_node;
+	while(n){
+		if(!visited[n->data]){
+			DFS(g, n->data, visited);
+		}
+		n = n->next;
+	}
+	return;
+}
+
+void DFSOutprint(struct Graph *g){
+	int i, visited[100];
+	for(i = 0; i < g->vernum; i++){
+		visited[i] = 0;
+	}
+	printf("深度优先搜索：\n");
+	for(i = 0; i < g->vernum; i++){
+		if(!visited[i]){
+			DFS(g, i, visited);
+		}
+	}
+	printf("\n");
+}
+
+// 首先能够获取权值的 一定是一条边上的的两个顶点
+int getWeight(struct Graph *g, int start, int end){
+	struct BNode *node;
+	if (start == end)
+        return 0;
+    node = g -> node[start] -> first_node;
+    while(node != NULL){
+    	if(end == node -> data){
+    		return node -> weight;
+    	}
+    	node = node -> next;
+    }
+    return INF;
+}
+
+// 最短路径
+/*
+	start -- 起始顶点
+	 prev -- 前驱顶点数组
+	 dist -- 长度数组（dist[i] 是顶点 start 到顶点 i 的最短路径所经历的全部顶点中，位于顶点 i 之前的那个顶点）
+*/
+void dijkstra(struct Graph *g, int start, int prev[], int dist[]){
+	int i, j, k, min, tmp;
+	// flag 数组是用来表示顶点 start 到顶点 i 的最短路径是否已经被成功获取
+	int flag[100];
+	// 初始化
+	for(i = 0; i < g->vernum; i++){
+		flag[i] = 0;
+		prev[i] = 0;
+		// 把每个点到顶点 start 的距离都表示出来， 存储在 dist[i] 数组里
+		dist[i] = getWeight(g, start, i);
+	}
+	flag[start] = 1;
+	dist[start] = 0;
+	for(i = 1; i < g->vernum; i++){
+		min = INF;
+		for(j = 1; j < g->vernum; j++){
+			if(flag[j] == 0 && dist[j] < min){
+				min = dist[j];
+				k = j;
+			}
+		}
+		flag[k] = 1;
+		for(j = 0; j < g->vernum; j++){
+			tmp = getWeight(g, k, j);
+			tmp = (tmp==INF ? INF : (min + tmp)); // 防止溢出
+			if (flag[j] == 0 && (tmp  < dist[j]) ){
+                dist[j] = tmp;
+                prev[j] = k;
+            }
+		}
+	}
+	printf("dijkstra(%c): \n", g->node[start]->ch);
+    for (i = 0; i < g->vernum; i++)
+        printf("  shortest(%c, %c)=%d\n", g->node[start]->ch, g->node[i]->ch, dist[i]);
+}
+
 int main(){
 	struct Graph *g;
+	int prev[100] = {0};
+    int dist[100] = {0};
 	g = creatGraph();
 	printGraph(g);
+	DFSOutprint(g);
+	dijkstra(g, 0, prev, dist);
 	return 0;
 }
 
